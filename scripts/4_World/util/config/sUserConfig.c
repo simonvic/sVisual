ab6b48d62afed3f3ebdd404b0cb0ab72214358f0
simonvic <simonvic@live.it>
2021-04-13 17:53:30 +0200

    Completed SUserConfig
    It now creates the default file if missing

--- scripts/4_World/util/config/sUserConfig.c
+++ scripts/4_World/util/config/sUserConfig.c
@@ -1,21 +1,21 @@
 class SUserConfig{
 	
 	static const string USER_CONFIG_PATH = "$saves:\\sVisual\\config\\config.json";
-	static const string DEFAULT_USER_CONFIG_PATH = "$saves:\\sVisual\\config\\default.json";
+	static const string DEFAULT_USER_CONFIG_PATH = "$profile:\\sVisual\\config\\default.json";
 	
 	private static ref JsonFileLoader<SUserConfig> jsonLoader = new JsonFileLoader<SUserConfig>();
 	
-	SUserConfigBaseBuilding module_baseBuilding;
-	SUserConfigCaves module_caves;
-	SUserConfigCharacter module_character;
-	SUserConfigDriving module_driving;
-	SUserConfigInventory module_inventory;
-	SUserConfigLooting module_looting;
-	SUserConfigMedical module_medical;
-	SUserConfigPvP module_pvp;
-	SUserConfigSoftSkills module_softSkills;
-	SUserConfigSurvival module_survival;
-	SUserConfigVisual module_visual;
+	ref SUserConfigBaseBuilding module_baseBuilding = new SUserConfigBaseBuilding;
+	ref SUserConfigCaves module_caves               = new SUserConfigCaves;
+	ref SUserConfigCharacter module_character       = new SUserConfigCharacter;
+	ref SUserConfigDriving module_driving           = new SUserConfigDriving;
+	ref SUserConfigInventory module_inventory       = new SUserConfigInventory;
+	ref SUserConfigLooting module_looting           = new SUserConfigLooting;
+	ref SUserConfigMedical module_medical           = new SUserConfigMedical;
+	ref SUserConfigPvP module_pvp                   = new SUserConfigPvP;
+	ref SUserConfigSoftSkills module_softSkills     = new SUserConfigSoftSkills;
+	ref SUserConfigSurvival module_survival         = new SUserConfigSurvival;
+	ref SUserConfigVisual module_visual             = new SUserConfigVisual;
 	
 	/**
 	*	@brief Get a copy of user configuration file
@@ -28,8 +28,8 @@ class SUserConfig{
 			if(FileExist(DEFAULT_USER_CONFIG_PATH) && isValid(DEFAULT_USER_CONFIG_PATH)){
 				CopyFile(DEFAULT_USER_CONFIG_PATH, USER_CONFIG_PATH);
 			}else{
-				SLog.c("Couldn't load neither user config [ " + USER_CONFIG_PATH + " ] nor default config [ " + DEFAULT_USER_CONFIG_PATH + " ]");
-				return null;
+				SLog.c("Couldn't load neither user config [ " + USER_CONFIG_PATH + " ] nor default config [ " + DEFAULT_USER_CONFIG_PATH + " ]", "SUserConfig");
+				return createDefault(true);
 			}
 		}
 
@@ -41,10 +41,28 @@ class SUserConfig{
 	}
 	
 	/**
-	*	@brief Save the 
+	*	@brief Save the configuration
 	*/
 	void save(){
 		jsonLoader.JsonSaveFile(USER_CONFIG_PATH, this);
+	}
+	
+	/**
+	*	@brief Create the default configuration file
+	*	 @param verbose \p bool - Be verbose with logs
+	*/
+	private static SUserConfig createDefault(bool verbose = false){
+		SLog.i("Creating new default config file...", "SUserConfig", 1, verbose);
+		SUserConfig newDefault = new SUserConfig();
+		SFileHelper.touch(DEFAULT_USER_CONFIG_PATH);
+		jsonLoader.JsonSaveFile(DEFAULT_USER_CONFIG_PATH, newDefault);
+		SLog.i("Done", "", 2, verbose);
+		
+		SLog.i("Creating new config file...", "SUserConfig", 1, verbose);
+		newDefault.save();
+		SLog.i("Done", "", 2, verbose);
+		
+		return newDefault;
 	}
 	
 	/**
@@ -68,9 +86,9 @@ class SUserConfigPvP{}
 class SUserConfigSoftSkills{}
 class SUserConfigSurvival{}
 class SUserConfigVisual{
-	float ddofIntensity;
-	float headbobIntensity;
-	float motionBlurIntensity;
-	float bloomIntensity;
-	float headLeanAngle;
+	float ddofIntensity = 20.0;
+	float headbobIntensity = 1.0;
+	float motionBlurIntensity = 0.01;
+	float bloomIntensity = 0;
+	float headLeanAngle = 15;
 }
