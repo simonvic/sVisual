1342186d600741a0ef896edde58f19fd0ada68ab
simonvic <simonvic@live.it>
2021-04-16 13:18:28 +0200

    Fixed blood loss animation + adjusted deactivation condition
    It was not possible to call GetHealth on client

--- scripts/4_World/module_visual/ppeffects/BloodLoss.c
+++ scripts/4_World/module_visual/ppeffects/BloodLoss.c
@@ -4,13 +4,16 @@ modded class BloodLoss{
 	
 	override void OnUpdateClient(PlayerBase player, float deltatime){
 		//super.OnUpdateClient(player, deltatime);
-		
+				
 		//override saturation effect
 		if( player.GetTransferValues() && player.GetTransferValues().GetBlood() != m_BloodSet ) { //better to rewrite this, so we don't break other mods
 			m_BloodSet = player.GetTransferValues().GetBlood();
-			
-			ppeBloodLoss.setSaturation( Math.Clamp(SMath.mapTo(player.GetHealth("GlobalHealth","Blood"), 3000, 5000, 0, 1), 0, 1) );
-			PPEManager.activate(ppeBloodLoss);
+			ppeBloodLoss.setSaturation( player.GetTransferValues().GetBlood() );
+			if(m_BloodSet < 1){
+				PPEManager.activate(ppeBloodLoss);
+			}else{
+				PPEManager.deactivate(ppeBloodLoss);
+			}
 		}
 	}
 
