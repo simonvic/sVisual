aec677847ab5c877d67477aa7f93e93b3bb399aa
simonvic <simonvic.dev@gmail.com>
2021-05-27 10:51:35 +0200

    Reduced night vision vignette strength

--- scripts/3_Game/module_visual/ppeffects/presets/miscPresets.c
+++ scripts/3_Game/module_visual/ppeffects/presets/miscPresets.c
@@ -16,6 +16,6 @@ class PPENightVision : PPEMaterialPresetBase{
 	override void onInit(){
 		setFilmGrain(5.0, 1.5);
 		setColorization(PPEManager.getPPEColor( -0.5, 0.5, -0.5, 0.0));
-		setVignette(1.2, PPEManager.getPPEColor(0, 0, 0, 0));
+		setVignette(1.0, PPEManager.getPPEColor(0, 0, 0, 0));
 	}
 }
