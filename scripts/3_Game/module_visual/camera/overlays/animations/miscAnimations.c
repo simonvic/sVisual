812d6808030d2246608e6f32f265bbffaadfb6ae
simonvic <simonvic.dev@gmail.com>
2021-08-04 17:36:05 +0200

    Added spawn animation file

---
+++ scripts/3_Game/module_visual/camera/overlays/animations/miscAnimations.c
@@ -0,0 +1,15 @@
+class SCOTimedSpawn : SCameraOverlayTimed {
+	
+	override void onInit(){
+		setImage("MyMODS/sVisual/GUI/textures/overlays/black_4x4.edds");
+		setMask(SCOMasks.CIRCULAR);
+		setMaskTransitionWidth(1);
+		setDuration(1.3);
+		setSize(1.5, 1);
+		setPriority(eSCOPriority.EYE);
+	}
+	
+	override void onAnimate(float deltaTime){
+		setMaskProgress(Math.Sin(getTime() + Math.PI_HALF));
+	}
+}
