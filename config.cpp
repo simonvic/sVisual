696aaad76ec388b047e353f66a33b721c14a5b52
simonvic <simonvic.dev@gmail.com>
2022-03-24 14:06:34 +0100

    Remove unnecessary config declarations

--- config.cpp
+++ config.cpp
@@ -58,30 +58,9 @@ class CfgMods {
 };
 
 class CfgVehicles{
-	class SCameraOverlay {
-		image="";                                 // Resource image path, can be whatever an ImageWidget accepts texture
-		alpha=1.0;                                // [0.0 - 1.0] Alpha value (transparency)
-		mask="";                                  // Resource image path, can be whatever an ImageWidget accepts as mask
-		maskProgress=1.0;                         // [0.0 - 1.0] Mask progress
-		maskTransitionWidth=1.0;                  // Mask transition width (used as progress + transitionWidth)
-		position[] = {0.0, 0.0};                  // [0.0 - 1.0] X and Y position in screenspace
-		size[] = {1.0, 1.0};                      // [0.0 - 1.0] X and Y size in screenspace
-		rotation[] = {0.0, 0.0, 0.0};             // Yaw, Pitch and Roll defined in degrees
-		priority = 0;                             // Higher priority means closer to the camera (also known as z-depth)
-		targetCameras[] = {"DayZPlayerCamera"};   // Camera typename on which the overlay will be visible
-		hidesWithIngameHUD = 0;                   // [0 = false, 1 = true] Determines if it must hides when the player hides the ingame HUD
-	};
 
-	class SCameraOverlay_Eyewear : SCameraOverlay {
-		priority = 2;
-		targetCameras[] = {"DayZPlayerCamera1stPerson", "DayZPlayerCameraIronsights"};
-	};
-
-	class SCameraOverlay_Headgear : SCameraOverlay {
-		priority = 1;
-		targetCameras[] = {"DayZPlayerCamera1stPerson", "DayZPlayerCameraIronsights"};
-	};
-
+	class SCameraOverlay_Headgear;
+	class SCameraOverlay_Eyewear;
 
 	class MotoHelmet_ColorBase {
 		class sUDE {
@@ -176,59 +155,6 @@ class CfgVehicles{
 };
 
 class Cfg_sUDE{
-	class BaseBuilding{
-		enabled = 0;
-	};
-	class Caves{
-		enabled = 0;
-	};
-	class Character{
-		enabled = 0;
-	};
-	class Driving{
-		enabled = 0;
-	};
-	class Inventory{
-		enabled = 0;
-	};
-	class Looting{
-		enabled = 0;
-	};
-	class Medical{
-		enabled = 0;
-	};
-	class Gunplay{
-		enabled = 0;
-	};
-	class SoftSkills{
-		enabled = 0;
-	};
-	class Survival{
-		enabled = 0;
-		class Cooking{
-			class Recipes{
-				minFluidity = 0;
-				maxFluidity = 9999;
-				class Liquids{
-					minFluidity = 500;
-					class Drinks{
-						maxFluidity = 9999;
-						class CoffeeRecipe{};
-					};
-					class Soups{
-						maxFluidity = 1000;
-						class MushroomSoupRecipe{};
-					};
-				};
-				class Solids{
-					minFluidity = 0;
-					maxFluidity = 500;
-					class CookedRiceRecipe{};
-					class RiceAndMushroomsRecipe{};
-				};
-			};
-		};
-	};
 	class Visual{
 		enabled = 1;
 		class Camera{
