d555f4092849b349e42bd869c213e88ed99cad18
simonvic <simonvic.dev@gmail.com>
2022-05-05 15:52:02 +0200

    Fix camera manager not updating after DayZ 1.17.154754

--- scripts/4_World/module_visual/camera/DayZPlayerCamera1stPerson.c
+++ scripts/4_World/module_visual/camera/DayZPlayerCamera1stPerson.c
@@ -16,7 +16,14 @@ modded class DayZPlayerCamera1stPerson{
 	override float getLeanRollAngle(){
 		//this is needed to change the roll angle based on freelook
 		return m_iPlayer.m_MovementState.m_fLeaning * Math.Lerp(m_camManager.getHeadLeanAngle(),- m_camManager.getHeadLeanAngle(), Math.AbsFloat(m_fLeftRightAngle) / ( CONST_LR_MAX / 2) );
-	}	
+	}
+	
+	override void OnUpdate(float pDt, out DayZPlayerCameraResult pOutResult) {
+		super.OnUpdate(pDt, pOutResult);
+		if (m_camManager.canUpdateFrom(this)) {
+			m_camManager.onUpdate(pDt, pOutResult);
+		}
+	}
 	
 	
 }
